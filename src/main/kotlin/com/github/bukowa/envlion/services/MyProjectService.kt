335ffb5c5e8fad88ccf426f51ea4e518df6b4f6e
GitHub Action <action@github.com>
2025-02-15 20:59:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/bukowa/envlion/services/MyProjectService.kt
+++ src/main/kotlin/com/github/bukowa/envlion/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.bukowa.envlion.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.bukowa.envlion.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
